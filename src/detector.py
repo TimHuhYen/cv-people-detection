e2872a2a68acf628aef887024a566a09b995d683
TimmtyN <nguyentimot@gmail.com>
2026-02-09 20:39:07 -0500

    Integrate YOLOv8 for real time person detection.

---
+++ src/detector.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+class PersonDetector:
+    def __init__(self, model_path="yolov8n.pt", conf=0.4):
+        # loads pretrainded model as class
+        self.model = YOLO(model_path)
+        # saves confidence score
+        self.conf = conf
+
+    def detect (self, frame):
+        results = self.model(frame, conf=self.conf, verbose=False)
+        detections = []
+
+        for r in results:
+            for box in r.boxes:
+                cls = int(box.cls[0])
+                if cls ==0: # coco class 0 = person
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    score = float(box.conf[0])
+                    detections.append((x1, y1, x2, y2, score))
+
+        return detections
--- src/main.py
+++ src/main.py
@@ -1,4 +1,37 @@
+import cv2
 from camera import run_camera
+from detector import PersonDetector
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = PersonDetector()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        detections = detector.detect(frame)
+
+        for (x1, y1, x2, y2, score) in detections:
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(
+                frame,
+                f"Person {score:.2f}",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2
+            )
+
+        cv2.imshow("People Detection", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    run_camera()
+    main()
