0457821d139f02334e6466114afeec9cb3d31eb7
TimmtyN <nguyentimot@gmail.com>
2026-02-10 11:11:05 -0500

    Added frame scaling for performance scaling

--- src/main.py
+++ src/main.py
@@ -3,7 +3,27 @@ from camera import run_camera
 from detector import PersonDetector
 from utils.fps import FPSCounter
 
+
+"""
+    Captures each frame, applies YOLOv8n, COCO detects, infer position [0]- people
+    Added:
+        - fps counte for performance scale
+        - len(detections) for people counting
+
+    TODO:
+        - reduce the pixel count of frames
+            - reduce data transfer and preprocessing cost
+            - or control model input resolution
+
+        [extract meaning, reason, and behavior]
+        - assign ID's to track people
+        - set a persons in/out area
+
+        - seperate panel for UI
+
+"""
 def main():
+    SCALE = 0.5
     cap = cv2.VideoCapture(0)
     detector = PersonDetector()
     fps_counter = FPSCounter() # new fpsCounter
@@ -13,12 +33,24 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
-            
-        detections = detector.detect(frame) # "see" and draw bbox
+        
+        h, w = frame.shape[:2]
+        SCALED_frame = cv2.resize(
+            frame, 
+            (int(w * SCALE), int(h * SCALE))
+        )
+
+        detections = detector.detect(SCALED_frame) # "infer" and draw bbox
         fps = fps_counter.update() # update fps once per frame
         person_count = len(detections)
 
         for (x1, y1, x2, y2, score) in detections:
+
+            x1 = int(x1 / SCALE)
+            y1 = int(y1 / SCALE)
+            x2 = int(x2 / SCALE)
+            y2 = int(y2 / SCALE)
+
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(
                 frame,
